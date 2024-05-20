6764ec39fe77ce11492ac8fdafa8a7a1b0f44693
ansh4223 <ak210073@gmail.com>
2024-05-20 20:35:55 +0530

    Feat: Added copy link functionality and alert for copied

--- vox_sync_meet/src/app/CreateMeetingPage.tsx
+++ vox_sync_meet/src/app/CreateMeetingPage.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
-import { Call, MemberRequest, useStreamVideoClient } from "@stream-io/video-react-sdk";
-import { Loader2 } from "lucide-react";
+import {
+  Call,
+  MemberRequest,
+  useStreamVideoClient,
+} from "@stream-io/video-react-sdk";
+import { Copy, Loader2 } from "lucide-react";
 import { useState } from "react";
 import { getUserIds } from "./actions";
 import Button from "@/components/Button";
+import Link from "next/link";
 export default function CreateMeetingPage() {
   const [descriptionInput, setDescriptionInput] = useState("");
   const [startTimeInput, setStartTimeInput] = useState("");
@@ -16,27 +21,29 @@ export default function CreateMeetingPage() {
 
   const { user } = useUser();
 
-  async function createMeeting(){
-    if(!client || !user){
+  async function createMeeting() {
+    if (!client || !user) {
       return;
     }
-    try{
+    try {
       const id = crypto.randomUUID();
 
-      const callType = participantsInput ? "private-meeting" : "default"
+      const callType = participantsInput ? "private-meeting" : "default";
 
       const call = client.call(callType, id);
 
-      const memberEmails = participantsInput.split(",").map(email => email.trim());
+      const memberEmails = participantsInput
+        .split(",")
+        .map((email) => email.trim());
 
       const memberIds = await getUserIds(memberEmails);
 
       const members: MemberRequest[] = memberIds
-      .map(id => ({user_id: id, role: "call_member"}))
-      .concat({user_id: user.id, role: "call_member"})
-      .filter(
-        (v, i, a) => a.findIndex((v2) => v2.user_id === v.user_id) === i
-      )
+        .map((id) => ({ user_id: id, role: "call_member" }))
+        .concat({ user_id: user.id, role: "call_member" })
+        .filter(
+          (v, i, a) => a.findIndex((v2) => v2.user_id === v.user_id) === i,
+        );
 
       const starts_at = new Date(startTimeInput || Date.now()).toISOString();
 
@@ -44,15 +51,14 @@ export default function CreateMeetingPage() {
         data: {
           starts_at,
           members,
-          custom: {description: descriptionInput}
-        }
-      })
+          custom: { description: descriptionInput },
+        },
+      });
 
       setCall(call);
-
-    }catch(error){
+    } catch (error) {
       console.error(error);
-      alert("Something went wrong. Please try again later.")
+      alert("Something went wrong. Please try again later.");
     }
   }
 
@@ -75,7 +81,7 @@ export default function CreateMeetingPage() {
           value={participantsInput}
           onChange={setParticipantsInput}
         />
-        <Button onClick={createMeeting}className="w-full">
+        <Button onClick={createMeeting} className="w-full">
           Create meeting
         </Button>
       </div>
@@ -223,15 +229,30 @@ function ParticipantsInput({ value, onChange }: ParticipantsInputProps) {
   );
 }
 
-
 interface MeetingLinkProps {
   call: Call;
 }
 
-function MeetingLink({call}: MeetingLinkProps){
-  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${call.id}`
+function MeetingLink({ call }: MeetingLinkProps) {
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${call.id}`;
 
-  return <div className="text-center">
-    {meetingLink}
-  </div>;
+  return (
+    <div className="flex flex-col items-center gap-3 text-center">
+      <div className="flex items-center gap-3">
+        <span>
+          Invitation link:{" "}
+          <Link href={meetingLink} className="font-medium">
+            {meetingLink}
+          </Link>
+        </span>
+        <button title="Copy invitation link"
+        onClick={() => {
+          navigator.clipboard.writeText(meetingLink)
+          alert("Copied to clipboard");
+        }}>
+        <Copy />
+        </button>
+      </div>
+    </div>
+  );
 }
