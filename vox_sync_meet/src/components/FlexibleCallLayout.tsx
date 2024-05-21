6e5c0dccc0af51316cf7a29a161a69e8ce8241c5
ansh4223 <ak210073@gmail.com>
2024-05-21 21:00:58 +0530

    Feat: colored selected layout button

--- vox_sync_meet/src/components/FlexibleCallLayout.tsx
+++ vox_sync_meet/src/components/FlexibleCallLayout.tsx
@@ -24,13 +24,19 @@ interface CallLayoutButtonProps{
 function CallLayoutButton({layout, setLayout}: CallLayoutButtonProps){
     return <div className="mx-auto w-fit space-x-6" >
         <button onClick={() => setLayout("speaker-vert")}>
-            <BetweenVerticalEnd />
+            <BetweenVerticalEnd 
+            className={layout !== "speaker-vert" ? "text-gray-400" : ""}
+            />
         </button>
         <button onClick={() => setLayout("speaker-horiz")}>
-            <BetweenHorizonalEnd />
+            <BetweenHorizonalEnd 
+            className={layout !== "speaker-horiz" ? "text-gray-400" : ""}
+            />
         </button>
         <button onClick={() => setLayout("grid")}>
-            <LayoutGrid />
+            <LayoutGrid 
+            className={layout !== "grid" ? "text-gray-400" : ""}
+            />
         </button>
     </div>
 }
