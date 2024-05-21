7f046b2d2372fafb57893109f617b6ad29c76abf
ansh4223 <ak210073@gmail.com>
2024-05-21 20:54:38 +0530

    Feat: Toggle call layout to horizontal, vertical and grid

--- vox_sync_meet/src/components/FlexibleCallLayout.tsx
+++ vox_sync_meet/src/components/FlexibleCallLayout.tsx
@@ -1,5 +1,6 @@
 import useStreamCall from "@/hooks/useStreamCall";
 import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import { BetweenHorizonalEnd, BetweenVerticalEnd, LayoutGrid } from "lucide-react";
 import { useState } from "react";
 
 type CallLayout = "speaker-vert" | "speaker-horiz" | "grid";
@@ -10,11 +11,29 @@ export default function FlexibleCallLayout(){
     const call = useStreamCall();
 
     return <div className="space-y-3">
+        <CallLayoutButton layout={layout} setLayout={setLayout} />
         <CallLayoutView layout={layout} />
     </div>
 }
 
+interface CallLayoutButtonProps{
+    layout: CallLayout,
+    setLayout: (layout: CallLayout) => void,
+}
 
+function CallLayoutButton({layout, setLayout}: CallLayoutButtonProps){
+    return <div className="mx-auto w-fit space-x-6" >
+        <button onClick={() => setLayout("speaker-vert")}>
+            <BetweenVerticalEnd />
+        </button>
+        <button onClick={() => setLayout("speaker-horiz")}>
+            <BetweenHorizonalEnd />
+        </button>
+        <button onClick={() => setLayout("grid")}>
+            <LayoutGrid />
+        </button>
+    </div>
+}
 
 interface CallLayoutViewProps{
     layout: CallLayout
